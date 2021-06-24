6e840ea83f4d63e188ca6e3ae12b742886536506
DennisWanjeri <shikandadennis07@gmail.com>
2021-06-24 03:51:15 +0000

    basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -14,3 +14,26 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
+        fmt = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs is not None:
+            for k, v in kwargs.items():
+                if k == "updated_at" or k == "created_at":
+                    self.__dict__[k] = datetime.strptime(v, fmt)
+                else:
+                    self.__dict__[k] = v
+    def save(self):
+        """update updated_at attribute with current datetime"""
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """returns BaseModel dictionary instance"""
+        Bdict = self.__dict__.copy()
+        Bdict["created_at"] = self.created_at.isoformat()
+        Bdict["updated_at"] = self.updated_at.isoformat()
+        Bdict["__class__"] = self.__class__.__name__
+        return Bdict
+
+    def __str__(self):
+        """returns a string representation of an instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__,
+                                     self.id, self.__dict__)
