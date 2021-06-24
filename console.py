aef7085ea04f6b8591c8741e343317a235c35b30
DennisWanjeri <shikandadennis07@gmail.com>
2021-06-24 12:56:30 +0000

    cmd functions

--- console.py
+++ console.py
@@ -57,7 +57,8 @@ class HBNBCommand(cmd.Cmd):
     def do_destroy(self, arg):
         """Usage: destroy <class_name> <id>
         Deletes an instance based on the class name and id
-        and savea the changes to a json file"""
+        and save the changes to a json file"""
+        obj_dict = storage.all()
         if len(arg) == 0:
             print("** class name missing **")
         elif arg[0] not in HBNBCommand.__classes:
@@ -70,7 +71,34 @@ class HBNBCommand(cmd.Cmd):
             del obj_dict["{}.{}".format(arg[0], arg[1])]
             storage.save()
 
-    def do_all(self,arg):
+    def do_all(self, arg):
+        """Usage: all <class>, class is optional
+        Prints all string representation of all instances
+        based or not on the class name.
+        """
+        if len(arg) > 0 and arg[0] not in HBNBCommand.__classes:
+            print("** class doesn't exist **")
+        else:
+            obj_list = []
+            for obj in storage.all().values():
+                if len(arg) > 0 and arg[0] == obj.__class__.__name__:
+                    obj_list.append(obj.__str__())
+                elif len(arg) == 0:
+                    obj_list.append(obj.__str__())
+            print(obj_list)
+
+    def do_update(self, arg):
+        """Usage: update <class name> <id> <attribute name>
+        "<attribute value>"
+        Updates an instance based on the class name and id
+        by adding or updating attribute
+        """
+        if len(arg) == 0:
+            print("** class name missing **")
+        for obj in storage.all().values():
+            if len(arg) > 0 and arg[0] not in HBNBCommand.__classes:
+                print("** class doesn't exist **")
                 
+
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
